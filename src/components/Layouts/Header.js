e3dc7d0b52b7593908d5e319756540c9e218c58e
ARISH <mohdarish2001@gmail.com>
2025-07-30 01:18:57 +0530

    Fixed issue

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,43 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/logo/logo.png";
 import "../../styles/HeaderStyle.css";
 
 const Header = () => {
   const [nav, setNav] = useState(false);
+  const location = useLocation();
 
-  // Scroll Navbar
+  // Scroll Navbar Sticky
   const changeValueOnScroll = () => {
     const scrollValue = document?.documentElement?.scrollTop;
-    scrollValue > 100 ? setNav(true) : setNav(false);
+    setNav(scrollValue > 100);
   };
 
-  window.addEventListener("scroll", changeValueOnScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", changeValueOnScroll);
+    return () => window.removeEventListener("scroll", changeValueOnScroll);
+  }, []);
+
+  // Close navbar collapse on route change (for mobile)
+  useEffect(() => {
+    const navCollapse = document.querySelector(".navbar-collapse");
+    if (navCollapse?.classList.contains("show")) {
+      navCollapse.classList.remove("show");
+    }
+  }, [location]);
 
   return (
     <header>
       <Navbar
         collapseOnSelect
         expand="lg"
-        className={`${nav === true ? "sticky" : ""}`}
+        className={`${nav ? "sticky" : ""}`}
       >
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand>
             <Link to="/" className="logo">
               <img src={Logo} alt="Logo" className="img-fluid" />
             </Link>
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#menu">Our Menu</Nav.Link>
-              <Nav.Link href="#shop">Shop</Nav.Link>
-              <Nav.Link href="#blog">Blog</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-              <Nav.Link href="#cart">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
+              <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/">
                 <div className="cart">
                   <i className="bi bi-bag fs-5"></i>
                   <em className="roundpoint">2</em>
