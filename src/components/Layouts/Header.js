323ea0d069e5a2a41b1028cccfe70c548facac58
ARISH <mohdarish2001@gmail.com>
2025-08-21 16:26:57 +0530

    new commit

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 import Logo from "../../assets/logo/logo.png";
 import "../../styles/HeaderStyle.css";
 
@@ -27,13 +28,11 @@ const Header = () => {
     }
   }, [location]);
 
+  const HEADER_OFFSET = -80; // Adjust this value to match your header height
+
   return (
     <header>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        className={`${nav ? "sticky" : ""}`}
-      >
+      <Navbar collapseOnSelect expand="lg" className={`${nav ? "sticky" : ""}`}>
         <Container>
           <Navbar.Brand>
             <Link to="/" className="logo">
@@ -44,16 +43,64 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/about">About</Nav.Link>
-              <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
-              <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
-              <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="home"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="about"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                About
+              </Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="menu"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                Our Menu
+              </Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="shop"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                Shop
+              </Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="blog"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                Blog
+              </Nav.Link>
+              <Nav.Link
+                as={ScrollLink}
+                to="contact"
+                offset={HEADER_OFFSET}
+                smooth={true}
+                duration={200}
+              >
+                Contact
+              </Nav.Link>
               <Nav.Link as={Link} to="/">
                 <div className="cart">
                   <i className="bi bi-bag fs-5"></i>
-                  <em className="roundpoint">2</em>
+                  <em className="roundpoint">0</em>
                 </div>
               </Nav.Link>
             </Nav>
@@ -63,5 +110,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;
